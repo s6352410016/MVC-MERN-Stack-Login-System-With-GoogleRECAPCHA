19073acbd76c69b862f1eba3aaa234a79fc7d00f
s6352410016 <s6352410016@sau.ac.th>
2023-01-12 18:37:50 +0700

    Last Updated On 12/01/2023 18:37

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname , './client/build')));
-    app.get('*' , (req , res) => {
-        res.sendFile(path.join(__dirname , './client/build/index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname , './client/build')));
+app.get('*' , (req , res) => {
+    res.sendFile(path.join(__dirname , './client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 5000 , () => {
     console.log('Starting server...');
